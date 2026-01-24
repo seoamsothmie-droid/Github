f9314659e62067d7978d5c72e6bd29fc0fbe2fd2
Sothmie <SeoamSothmie.outlook.com>
2026-01-24 14:19:09 +0700

    Test geihub

---
+++ EX2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<h2>HTML Form using POST</h2>
+<form method="Post" action="POST.php">
+<table>
+<tr>
+<td>Name : </td>
+<td><input type="text" name="txtname"></td>
+</tr>
+<tr>
+<td>Email : </td>
+<td><input type="email" name="txtemail"></td>
+</tr>
+<tr>
+<td colspan='2'>
+<input type="submit" name="submit" value="submit">
+<input type="reset" name="reset" value="reset">
+</td>
+</tr>
+</table>
+</form>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ Idex.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<h1>PHP Basic Syntax</h1>
+<hr>
+<p>
+<a href="Ex01.php">Example01</a> &nbsp;|&nbsp;
+<a href="Ex02.php">Example02</a> &nbsp;|&nbsp;
+<a href="Ex03.php">Example03</a> &nbsp;|&nbsp;
+<a href="Ex04.php">Example04</a> &nbsp;|&nbsp;
+<a href="Ex05.php">Example05</a>
+</p>
+</body>
+</html>
